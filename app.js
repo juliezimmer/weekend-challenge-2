f7f541993d8aaa132238e37305bd3cf79d7cb98e
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-08-05 22:14:14 -0500

    add styling for calculator

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ var bodyParser = bodyParser();
 
 var port = 5000;
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/*', function(req, res){
+    res.send('Request Received');
+});
+
 app.listen(5000, function(){
     console.log("listening on port:", port);
 })
 
-//app.get
--- public/client.js
+++ public/client.js
@@ -1,1 +1,15 @@
 console.log("the files are linked");
+console.log('client.js is working');
+
+/*$(document).ready(function(){
+    console.log('jquery is working');
+    $('#enterNumbersbutton').on('click', function(){
+        console.log('enterNumbersButton was clicked'); //I can't get this to confirm
+        var firstNumberBox = $('#firstNumberBox').val();
+        var secondNumberBox = $('#secondNumberBox').val();
+        var inputObject = {
+            numberOne: firstNumberBox,
+            numberTwo: secondNumberBox
+        };
+    });
+*/
